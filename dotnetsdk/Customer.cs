ec0b826856b401453468f02ae45b7f88d4894c53
dcamdupe <david@uberconcept.com>
2019-07-22 19:24:23 +1000

    Added support for the customer service

---
+++ dotnetsdk/Customer.cs
@@ -0,0 +1,13 @@
+﻿using evertech.sdk.Models;
+using evertech.sdk.Tools;
+
+namespace evertech.sdk
+{
+    public class Customer
+    {
+        public CustomerData CreateWithCard(NewCustomerCard newCustomer)
+        {
+            return HttpWrapper.Call<NewCustomerCard, CustomerData>("/customer/card", Method.POST, newCustomer);
+        }
+    }
+}
---
+++ dotnetsdk/Models/CustomerData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace evertech.sdk.Models
+{
+    public class CustomerData
+    {
+        public string CustomerId { get; set; }
+        public string Reference { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
