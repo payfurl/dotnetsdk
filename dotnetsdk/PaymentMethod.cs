2f7a78a815eb127f28682c234c22cc8e71791da6
dcamdupe <david@uberconcept.com>
2019-08-04 09:08:11 +1000

    Fixed class type returned

--- dotnetsdk/PaymentMethod.cs
+++ dotnetsdk/PaymentMethod.cs
@@ -7,11 +7,11 @@ namespace evertech.sdk
 {
     public class PaymentMethod
     {
-        public List<PaymentData> GetForCustomer(string customerId)
+        public List<PaymentMethod> GetForCustomer(string customerId)
         {
             customerId = HttpUtility.UrlEncode(customerId);
 
-            return HttpWrapper.Call<string, List<PaymentData>>("/payment_method/customer/" + customerId, Method.GET, null);
+            return HttpWrapper.Call<string, List<PaymentMethod>>("/payment_method/customer/" + customerId, Method.GET, null);
         }
     }
 }
