775f7b6028ae3bd4f36be145106c816060542009
dcamdupe <david@uberconcept.com>
2019-08-04 17:16:16 +1000

    Added first tests for customer

---
+++ FunctionalTests/Customer.cs
@@ -0,0 +1,45 @@
+﻿using evertech.sdk;
+using evertech.sdk.Models;
+using NUnit.Framework;
+
+namespace FunctionalTests
+{
+    [TestFixture]
+    public class Customer
+    {
+        [SetUp]
+        public void SetConfig()
+        {
+            Config.Setup("SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c", Environment.LOCAL);
+        }
+
+        [Test]
+        public void SearchWithValidReference()
+        {
+            var search = new CustomerSearch
+            {
+                Reference = "123123123"
+            };
+
+            var svc = new evertech.sdk.Customer();
+            var result = svc.Search(search);
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [Test]
+        public void SearchWithInvalidKey()
+        {
+            Config.Setup("invalidkey", Environment.LOCAL);
+
+            var search = new CustomerSearch
+            {
+                Reference = "123123123"
+            };
+
+            var svc = new evertech.sdk.Customer();
+
+            Assert.Throws<ApiException>(() => svc.Search(search));
+        }
+    }
+}
