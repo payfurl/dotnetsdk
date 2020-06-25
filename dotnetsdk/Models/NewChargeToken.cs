37dadd0c636224e266d62030bfc7d06b8df92a1a
dcamdupe <david@uberconcept.com>
2020-06-25 21:02:40 +1000

    Removed extra parameter

--- dotnetsdk/Models/NewChargeToken.cs
+++ dotnetsdk/Models/NewChargeToken.cs
@@ -4,7 +4,6 @@
     {
         public decimal Amount { get; set; }
         public string Currency { get; set; }
-        public string ProviderId { get; set; }
         public string Reference { get; set; }
         public string Token { get; set; }
     }
