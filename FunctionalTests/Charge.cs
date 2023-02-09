de27ed5f5c1565586cc0bc6f3d4f6086c07bdafc
Julia <juliaprogramming69@gmail.com>
2023-02-09 17:37:05 +0200

    Move functionality to WebhookTools

--- FunctionalTests/Charge.cs
+++ FunctionalTests/Charge.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using payfurl.sdk;
 using payfurl.sdk.Models;
 using Xunit;
@@ -199,22 +198,6 @@ namespace FunctionalTests
             var ex = await Assert.ThrowsAsync<ApiException>(Act);
 
             Assert.Equal(90, ex.Code);
-        }
-
-        [Theory]
-        [InlineData("rDYP2MxMKvvmoV2KrbOi4pnelHnVJoFYdBegvCK7IQk=", true)]
-        [InlineData("InvalidSignature", false)]
-        public async Task VerifySignatureForWebhook(string value, bool expected)
-        {
-            const string webhookSignatureKey = "dCM6l9ngZMJXVappk73yS607k1K7byfyzTTdToaKMa8=";
-
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://webhook.com");
-            request.Headers.Add("X-Payfurl-Signature", value);
-            request.Content = new StringContent("{\"data\":{\"chargeId\":\"3f83ab8fdf624c649bc70bbba81d6c2b\",\"providerChargeId\":\"ch_3MYd2tE9mXU4onpB0r5iTsiL\",\"amount\":20,\"providerId\":\"a26c371f-94f6-40da-add2-28ec8e9da8ed\",\"paymentInformation\":{\"paymentMethodId\":\"80da8c2d674b4d2e8c65a6520e89d070\",\"card\":{\"cardNumber\":\"4111********1111\",\"expiryDate\":\"12/25\",\"type\":\"VISA\",\"cardType\":\"CREDIT\",\"cardIin\":\"411\"},\"type\":\"CARD\"},\"customerId\":\"025c73d9cd0540e9a5a997f8ba97c732\",\"status\":\"SUCCESS\",\"dateAdded\":\"2023-02-06T22:20:19.0461561Z\",\"successDate\":\"2023-02-06T22:20:20.8655832Z\",\"estimatedCost\":0.20,\"estimatedCostCurrency\":\"AUD\",\"currency\":\"Aud\",\"refunds\":[],\"threeDsVerified\":false},\"meta\":{\"messageId\":\"bc4f056315d6e0205ab085dde45c4a46\",\"timestamp\":\"2023-01-19T20:37:12.8456589Z\",\"type\":\"transaction\",\"eventType\":\"transaction.status.changed\"}}");
-
-            var isFromPayFurl = await payfurl.sdk.Charge.IsFromPayFurl(request, webhookSignatureKey);
-
-            Assert.Equal(isFromPayFurl, expected);
         }
     }
 }
---
+++ dotnetsdk/WebhookValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace payfurl.sdk
+{
+    public static class WebhookTools
+    {
+        /// <summary>
+        /// Validate that a webhook event notification came from PayFURL.Requests that fail validation
+        /// should be discarded as they cannot be trusted.
+        /// </summary>
+        /// <param name="request">HTTP Request</param>
+        /// <param name="webhookSignatureKey">Webhook signature key (from dashboard)</param>
+        /// <returns>If the request from PayFURL returns true, otherwise false</returns>
+        public static async Task<bool> IsFromPayFurl(HttpRequestMessage request, string webhookSignatureKey)
+        {
+            var signature = request.Headers.GetValues("X-Payfurl-Signature").FirstOrDefault();
+            var requestBody = await request.Content.ReadAsStringAsync();
+
+            return IsValidWebhook(requestBody, signature, webhookSignatureKey);
+        }
+
+        private static bool IsValidWebhook(string requestBody, string signatureHeader, string signatureKey)
+        {
+            var requestBytes = Encoding.UTF8.GetBytes(requestBody);
+            var secret = Encoding.UTF8.GetBytes(signatureKey);
+
+            using (var hmac = new HMACSHA256(secret))
+            {
+                var hash = hmac.ComputeHash(requestBytes);
+                var hashString = Convert.ToBase64String(hash);
+
+                return hashString.Equals(signatureHeader);
+            }
+        }
+    }
+}
