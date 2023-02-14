5ce66968dd9840164edb7912abb3d43c1462208f
Julia <juliaprogramming69@gmail.com>
2023-02-14 09:03:54 +0200

    Add DeserializeTransaction method

--- FunctionalTests/WebhookTools.cs
+++ FunctionalTests/WebhookTools.cs
@@ -1,29 +1,40 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using payfurl.sdk.Models;
 using Xunit;
 
 namespace FunctionalTests
 {
     public class WebhookTools
     {
-        [Theory]
-        [InlineData("rDYP2MxMKvvmoV2KrbOi4pnelHnVJoFYdBegvCK7IQk=", true)]
-        [InlineData("InvalidSignature", false)]
-        public async Task VerifySignatureForWebhook(string value, bool expected)
+        [Fact]
+        public async Task DeserializeWebhookTransactionWithValidSignature()
         {
             const string webhookSignatureKey = "dCM6l9ngZMJXVappk73yS607k1K7byfyzTTdToaKMa8=";
 
             var request = new HttpRequestMessage(HttpMethod.Get, "https://webhook.com");
-            request.Headers.Add("X-Payfurl-Signature", value);
+            request.Headers.Add("X-Payfurl-Signature", "rDYP2MxMKvvmoV2KrbOi4pnelHnVJoFYdBegvCK7IQk=");
             request.Content = new StringContent("{\"data\":{\"chargeId\":\"3f83ab8fdf624c649bc70bbba81d6c2b\",\"providerChargeId\":\"ch_3MYd2tE9mXU4onpB0r5iTsiL\",\"amount\":20,\"providerId\":\"a26c371f-94f6-40da-add2-28ec8e9da8ed\",\"paymentInformation\":{\"paymentMethodId\":\"80da8c2d674b4d2e8c65a6520e89d070\",\"card\":{\"cardNumber\":\"4111********1111\",\"expiryDate\":\"12/25\",\"type\":\"VISA\",\"cardType\":\"CREDIT\",\"cardIin\":\"411\"},\"type\":\"CARD\"},\"customerId\":\"025c73d9cd0540e9a5a997f8ba97c732\",\"status\":\"SUCCESS\",\"dateAdded\":\"2023-02-06T22:20:19.0461561Z\",\"successDate\":\"2023-02-06T22:20:20.8655832Z\",\"estimatedCost\":0.20,\"estimatedCostCurrency\":\"AUD\",\"currency\":\"Aud\",\"refunds\":[],\"threeDsVerified\":false},\"meta\":{\"messageId\":\"bc4f056315d6e0205ab085dde45c4a46\",\"timestamp\":\"2023-01-19T20:37:12.8456589Z\",\"type\":\"transaction\",\"eventType\":\"transaction.status.changed\"}}");
 
             var signatureHeader = request.Headers.GetValues("X-Payfurl-Signature").FirstOrDefault();
             var requestBody = await request.Content.ReadAsStringAsync();
 
-            var isFromPayFurl = payfurl.sdk.WebhookTools.IsFormPayfurl(requestBody, signatureHeader, webhookSignatureKey);
+            var transaction = payfurl.sdk.WebhookTools.DeserializeTransaction(requestBody, signatureHeader, webhookSignatureKey);
+            
+            Assert.NotNull(transaction);
+            Assert.Equal(typeof(WebhookTransaction), transaction.GetType());
+        }
 
-            Assert.Equal(isFromPayFurl, expected);
+        [Fact]
+        public async Task DeserializeWebhookTransactionWithInvalidSignature()
+        {
+            const string webhookSignatureKey = "dCM6l9ngZMJXVappk73yS607k1K7byfyzTTdToaKMa8=";
+
+            var ex = Assert.Throws<ArgumentException>(() => payfurl.sdk.WebhookTools.DeserializeTransaction("", "InvalidSignature", webhookSignatureKey));
+
+            Assert.Equal("Request body is not from PayFURL", ex.Message);
         }
     }
 }
--- dotnetsdk/WebhookTools.cs
+++ dotnetsdk/WebhookTools.cs
@@ -1,20 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using payfurl.sdk.Models;
 
 namespace payfurl.sdk
 {
     public static class WebhookTools
     {
+        private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            NullValueHandling = NullValueHandling.Ignore,
+            PreserveReferencesHandling = PreserveReferencesHandling.None,
+            Formatting = Formatting.Indented,
+            Converters = new List<JsonConverter> { new StringEnumConverter() }
+        };
+
         /// <summary>
-        /// Validate that a webhook event notification came from PayFURL.Requests that fail validation
-        /// should be discarded as they cannot be trusted.
+        /// Deserialize webhook transaction request body from PayFURL and validate that a webhook event notification came from PayFURL. Requests that fail validation
+        /// will be discarded as they cannot be trusted.
         /// </summary>
         /// <param name="requestBody">HTTP request body</param>
         /// <param name="signatureHeader">Webhook signature header <code>X-Payfurl-Signature</code></param>
         /// <param name="signatureKey">Webhook signature key (from dashboard)</param>
-        /// <returns>If the request from PayFURL returns true, otherwise false</returns>
-        public static bool IsFormPayfurl(string requestBody, string signatureHeader, string signatureKey)
+        /// <returns>Deserialized transaction message</returns>
+        /// <exception cref="ArgumentException">Throw if signature validation is failed</exception>
+        public static WebhookTransaction DeserializeTransaction(string requestBody, string signatureHeader, string signatureKey)
+        {
+            if (!IsFormPayfurl(requestBody, signatureHeader, signatureKey))
+            {
+                throw new ArgumentException("Request body is not from PayFURL");
+            }
+
+            var transaction = JsonConvert.DeserializeObject<WebhookTransaction>(requestBody, JsonSettings);
+
+            return transaction;
+        }
+
+        private static bool IsFormPayfurl(string requestBody, string signatureHeader, string signatureKey)
         {
             var requestBytes = Encoding.UTF8.GetBytes(requestBody);
             var secret = Encoding.UTF8.GetBytes(signatureKey);
