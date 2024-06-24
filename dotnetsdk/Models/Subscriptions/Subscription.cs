da64f76c2d05cfb6b7b13c02ea303111281fb8c0
Eugene Mukhanov <eugene@hapbee.com>
2024-06-24 19:18:23 +0200

    PAY-3041

---
+++ dotnetsdk/Models/Subscriptions/Subscription.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace payfurl.sdk.Models.Subscriptions
+{
+
+    public class Subscription
+    {
+        public string SubscriptionId { get; set; }
+        public string PaymentMethodId { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public SubscriptionInterval Interval { get; set; }
+        public int? Frequency { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public SubscriptionEnd EndAfter { get; set; }
+        public SubscriptionRetryPolicy Retry { get; set; }
+        public WebhookConfig Webhook { get; set; }
+        public SubscriptionStatus Status { get; set; }
+
+        public enum SubscriptionStatus
+        {
+            Complete,
+            Active,
+            Cancelled
+        }
+
+        public enum SubscriptionInterval
+        {
+            Day,
+            Week,
+            Month,
+            Year
+        }
+    }
+}
---
+++ dotnetsdk/Subscription.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using payfurl.sdk.Helpers;
+using payfurl.sdk.Models.Subscriptions;
+using payfurl.sdk.Tools;
+
+namespace payfurl.sdk
+{
+    public class Subscription : ISubscription
+    {
+        public Task<payfurl.sdk.Models.Subscriptions.Subscription> CreateSubscriptionAsync(SubscriptionCreate subscriptionCreate)
+        {
+            return HttpWrapper.CallAsync<SubscriptionCreate, payfurl.sdk.Models.Subscriptions.Subscription>("/subscription/payment_method",
+                Method.POST, subscriptionCreate);
+        }
+
+        public payfurl.sdk.Models.Subscriptions.Subscription CreateSubscription(SubscriptionCreate subscriptionCreate)
+        {
+            return AsyncHelper.RunSync(() => HttpWrapper.CallAsync<SubscriptionCreate, payfurl.sdk.Models.Subscriptions.Subscription>("/subscription/payment_method",
+                Method.POST, subscriptionCreate));
+        }
+
+        public Task<payfurl.sdk.Models.Subscriptions.Subscription> GetSubscriptionAsync(string subscriptionId)
+        {
+            return HttpWrapper.CallAsync<string, payfurl.sdk.Models.Subscriptions.Subscription>($"/subscription/{subscriptionId}",
+                Method.GET, null);
+        }
+
+        public payfurl.sdk.Models.Subscriptions.Subscription GetSubscription(string subscriptionId)
+        {
+            return AsyncHelper.RunSync(() => HttpWrapper.CallAsync<string, payfurl.sdk.Models.Subscriptions.Subscription>($"/subscription/{subscriptionId}",
+                Method.GET, null));
+        }
+
+        public Task<payfurl.sdk.Models.Subscriptions.Subscription> DeleteSubscriptionAsync(string subscriptionId)
+        {
+            return HttpWrapper.CallAsync<string, payfurl.sdk.Models.Subscriptions.Subscription>($"/subscription/{subscriptionId}",
+                Method.DELETE, null);
+        }
+
+        public payfurl.sdk.Models.Subscriptions.Subscription DeleteSubscription(string subscriptionId)
+        {
+            return AsyncHelper.RunSync(() => HttpWrapper.CallAsync<string, payfurl.sdk.Models.Subscriptions.Subscription>($"/subscription/{subscriptionId}",
+                Method.DELETE, null));
+        }
+
+        public Task<SubscriptionList> SearchSubscriptionAsync(SubscriptionSearch search)
+        {
+            return HttpWrapper.CallAsync<SubscriptionSearch, SubscriptionList>("/subscription",
+                Method.POST, search);
+        }
+
+        public SubscriptionList SearchSubscription(SubscriptionSearch search)
+        {
+            return AsyncHelper.RunSync(() => HttpWrapper.CallAsync<SubscriptionSearch, SubscriptionList>("/subscription",
+                Method.POST, search));
+        }
+    }
+}
