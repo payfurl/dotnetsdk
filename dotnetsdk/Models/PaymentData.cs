220f16d26c1be7571010a273b982befbb93dae5f
Srini Vasudevan <srini@gofundraise.com>
2024-11-14 23:27:50 +1100

    added displaytype prop

--- dotnetsdk/Models/PaymentData.cs
+++ dotnetsdk/Models/PaymentData.cs
@@ -4,6 +4,7 @@
     {
         public CardData Card { get; set; }
         public string Type { get; set; }
+        public string DisplayType { get; set; }
         public string ProviderType { get; set; }
         public string Email { get; set; }
     }
