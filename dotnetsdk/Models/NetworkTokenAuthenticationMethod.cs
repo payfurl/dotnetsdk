e6e440bb5af597f3562a64fe7d02491c504e7ce4
Alex Ovseyenko <alex@payfurl.com>
2025-06-12 11:11:24 +1000

    PAY-4231: Rename API response field

---
+++ dotnetsdk/Models/NetworkTokenAuthenticationMethod.cs
@@ -0,0 +1,9 @@
+namespace payfurl.sdk.Models
+{
+    public class NetworkTokenAuthenticationMethod
+    {
+        public string AuthenticationMethodType { get; set; }
+        public string AuthenticationSubject { get; set; }
+        public UriData UriData { get; set; }
+    }
+}
