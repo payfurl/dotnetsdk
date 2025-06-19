d4acafb9d4e1ced3f9d566e5d9f217ac4c8342b3
Alex Ovseyenko <alex@payfurl.com>
2025-06-19 11:00:27 +1000

    PAY-4249: Add Metadata parameters to server SDKs when creating batch

--- dotnetsdk/Models/Batch/BatchData.cs
+++ dotnetsdk/Models/Batch/BatchData.cs
@@ -7,9 +7,11 @@ namespace payfurl.sdk.Models.Batch
     {
         public string BatchId { get; set; } 
         public int Count { get; set; } 
+        public int Success { get; set; } 
+        public int Failure { get; set; } 
         public string Description { get; set; } 
         public string Status { get; set; } 
-        public decimal Progress { get; set; } 
+        public decimal Progress { get; set; }
         public string Results { get; set; }
         public DateTime DateAdded { get; set; }
         public Dictionary<string, string> Metadata { get; set; }
